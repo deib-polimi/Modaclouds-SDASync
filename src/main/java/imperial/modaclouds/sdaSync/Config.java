811d12bb46e160734d5aacb1807cbd14aca5fe77
Riccardo B. Desantis <effetti@gmail.com>
2015-06-11 10:06:42 +0200

    Removed some warning.

--- src/main/java/imperial/modaclouds/sdaSync/Config.java
+++ src/main/java/imperial/modaclouds/sdaSync/Config.java
@@ -1,11 +1,6 @@
 package imperial.modaclouds.sdaSync;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 public class Config {
 
