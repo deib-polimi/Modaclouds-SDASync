4803dd27ce5c6d80b0af5454a8ba2fa54aedca3f
Weikun Wang <weikunwang89@gmail.com>
2015-06-11 18:05:26 +0100

    Update for Estimation functions

--- src/main/java/imperial/modaclouds/sdaSync/DataCollectorAgent.java
+++ src/main/java/imperial/modaclouds/sdaSync/DataCollectorAgent.java
@@ -4,9 +4,12 @@ import it.polimi.tower4clouds.common.net.UnexpectedAnswerFromServerException;
 import it.polimi.tower4clouds.data_collector_library.DCAgent;
 import it.polimi.tower4clouds.manager.api.ManagerAPI;
 import it.polimi.tower4clouds.model.data_collectors.DCDescriptor;
+import it.polimi.tower4clouds.model.ontology.InternalComponent;
 import it.polimi.tower4clouds.model.ontology.Resource;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -28,7 +31,7 @@ public class DataCollectorAgent  {
 	public DataCollectorAgent() {
 
 	}
-	
+
 	public Resource createResource(String resource){
 		Resource resources = new Resource(null,resource);
 		return resources;
@@ -50,6 +53,36 @@ public class DataCollectorAgent  {
 		dcAgent.start();
 	}
 
+	public Map<String,ArrayList<String>> checkEstimationMetric() {
+		Map<String,ArrayList<String>> metrics = new HashMap<String,ArrayList<String>>();
+		try {
+			Set<Resource> resources = manager.getResources();
+
+			while (resources == null) {
+				try {
+					Thread.sleep(1000);
+				} catch (Exception e) { }
+				resources = manager.getResources();
+			}
+			
+			int count = 1;
+			for(Resource resource : resources) {
+				if (resource instanceof InternalComponent) {
+					ArrayList<String> methods = new ArrayList<String>();
+					
+					methods.addAll(((InternalComponent) resource).getProvidedMethods());
+					metrics.put("instance"+count, methods);
+					count++;
+				}
+			}
+		}
+		catch (UnexpectedAnswerFromServerException | IOException e) {
+			e.printStackTrace();
+		}
+
+		return metrics; 
+	}
+
 	public Set<SDAMetric> checkMetric() {
 		Set<String> requiredMetrics = null;
 		Set<SDAMetric> sdaSet = new HashSet<SDAMetric>();
@@ -61,20 +94,20 @@ public class DataCollectorAgent  {
 		for (String requiredMetric : requiredMetrics) {
 			for (String supportedMetric: supportedMetrics) {
 				if (requiredMetric.startsWith(supportedMetric)) {
-					
+
 					SDAMetric sda = new SDAMetric();
-					
+
 					dcDescriptor.addMonitoredResource(requiredMetric,
 							new Resource()); // registering a data collector for
-											 // metric
-											 // Forecast_AverageFrontendCPU and
-											 // for all resources
+					// metric
+					// Forecast_AverageFrontendCPU and
+					// for all resources
 					dcAgent.refresh();  // required to be called when dcDescriptor is
-										// updated
-					
+					// updated
+
 					int index = requiredMetric.indexOf("_");
 					String subString = requiredMetric.substring(index+1);
-					
+
 					String metricToBeForecast = null;
 					if (subString.indexOf("_") == -1) {
 						metricToBeForecast = subString;
@@ -82,30 +115,30 @@ public class DataCollectorAgent  {
 					else {
 						metricToBeForecast = subString.substring(0,subString.indexOf("_"));
 					}
-					
+
 					//String metricToBeForecast = requiredMetric.substring(index+1);
 					System.out.println("Forecast required for metric "
 							+ metricToBeForecast);
-					
+
 					try {
 						manager.registerHttpObserver(metricToBeForecast, config.getSdaURL(),
 								"TOWER/JSON");
 					} catch (UnexpectedAnswerFromServerException | IOException e) {
 						e.printStackTrace();
 					} 						// An observer can
-											// be attached asking for different
-											// formats: RDF/JSON, TOWER/JSON,
-											// GRAPHITE,... TOWER/JSON is the new
-											// serialization format for sending
-											// monitoring data through the network
-											// replacing RDF/JSON. Example of json
-											// datum:
-											// {"resourceId": "frontend1", "metric":
-											// "AverageFrontendCPU", "value": 0.14,
-											// "timestamp": 123131231 }
+					// be attached asking for different
+					// formats: RDF/JSON, TOWER/JSON,
+					// GRAPHITE,... TOWER/JSON is the new
+					// serialization format for sending
+					// monitoring data through the network
+					// replacing RDF/JSON. Example of json
+					// datum:
+					// {"resourceId": "frontend1", "metric":
+					// "AverageFrontendCPU", "value": 0.14,
+					// "timestamp": 123131231 }
 					sda.setFunction(supportedMetric);
 					sda.setMetricName(requiredMetric);
-					
+
 					Map<String, String> params = dcAgent.getParameters(requiredMetric);
 					while (params == null) {
 						try {
@@ -113,11 +146,11 @@ public class DataCollectorAgent  {
 						} catch (Exception e) { }
 						params = dcAgent.getParameters(requiredMetric);
 					}
-					
+
 					sda.setParameters(params);
 					sda.setTargetMetric(metricToBeForecast);
 					sdaSet.add(sda);
-					
+
 					break;
 				}
 			}
